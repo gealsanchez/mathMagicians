e49ca0e4f119a662a82cc0e587fda6fe95b5f625
Uthmanbello <uthmanbello88@gmail.com>
2023-02-23 21:10:28 +0100

    Add tests for Calculator.js, Home.js and Quote.js files

---
+++ src/components/Quote.test.js
@@ -0,0 +1,6 @@
+import { render } from '@testing-library/react';
+import Quote from '../components/Quote';
+test('renders Quote component', () => {
+const { asFragment } = render(<Quote />);
+expect(asFragment()).toMatchSnapshot();
+});
